65ff8767288985b3d0bbe73b747ba6c6ca5ce762
Alisson Wenceslau <alisson.wenceslau@gmail.com>
2023-10-10 18:52:17 -0300

    Estado e cidade

---
+++ src/main/java/com/alissw/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,9 @@
+package com.alissw.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.alissw.cursomc.domain.Cidade;
+
+public interface CidadeRepository extends JpaRepository<Cidade, Integer>{
+
+}
---
+++ src/main/java/com/alissw/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,9 @@
+package com.alissw.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.alissw.cursomc.domain.Estado;
+
+public interface EstadoRepository extends JpaRepository<Estado, Integer>{
+
+}
