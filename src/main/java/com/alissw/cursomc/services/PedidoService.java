bd2bf76928270eea08585ba48e79da33dce298eb
Alisson Wenceslau <alisson.wenceslau@gmail.com>
2023-10-13 17:53:42 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/alissw/cursomc/services/PedidoService.java
@@ -0,0 +1,23 @@
+package com.alissw.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alissw.cursomc.domain.Pedido;
+import com.alissw.cursomc.repositories.PedidoRepository;
+import com.alissw.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repository;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repository.findById(id);
+		
+		return obj.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado: " + id + " Tipo: " + Pedido.class.getName()));
+	}
+}
