709677b698e6c4938edfc09d139821ac414ad736
Alisson Wenceslau <alisson.wenceslau@gmail.com>
2023-10-13 06:15:07 -0300

    ItemPedido e ItemPedidoPk

--- src/main/java/com/alissw/cursomc/domain/Pedido.java
+++ src/main/java/com/alissw/cursomc/domain/Pedido.java
@@ -2,7 +2,9 @@ package com.alissw.cursomc.domain;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
@@ -11,32 +13,36 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "pedidos")
-public class Pedido implements Serializable{
-	
+public class Pedido implements Serializable {
+
 	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private Date instant;
-	
-	@OneToOne(cascade = CascadeType.ALL, mappedBy = "pedido" )
+
+	@OneToOne(cascade = CascadeType.ALL, mappedBy = "pedido")
 	private Pagamento pagamento;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "cliente_id")
 	private Cliente cliente;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "endereco_de_entrega_id")
 	private Endereco enderecoDeEntrega;
-	
+
+	@OneToMany(mappedBy = "id.pedido")
+	private Set<ItemPedido> itens = new HashSet<>();
+
 	public Pedido() {
-		
+
 	}
 
 	public Pedido(Integer id, Date instant, Cliente cliente, Endereco enderecoDeEntrega) {
@@ -87,6 +93,14 @@ public class Pedido implements Serializable{
 		this.enderecoDeEntrega = enderecoDeEntrega;
 	}
 
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
---
+++ src/main/java/com/alissw/cursomc/repositories/ItemPedidoRepository.java
@@ -0,0 +1,9 @@
+package com.alissw.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.alissw.cursomc.domain.ItemPedido;
+
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer>{
+
+}
